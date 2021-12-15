b5611dae29dd5f1d2ee5b1f0387ae4649322370f
Yessir4253 <67040509+Yessir4253@users.noreply.github.com>
2021-12-15 17:52:25 +0800

    12/15 - started organization

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,4 +1,14 @@
-#include "auton.hpp"
+#include "main.h"
+
+void wingGrab() {
+    wings.set_value(true);
+    translate(-3.5_ft);
+    while(translatePID->getError() > 50) {
+        pros::delay(20);
+    }
+    wings.set_value(false);
+    translate(2.5_ft);
+}
 
 void skill(){
 
--- src/main.cpp
+++ src/main.cpp
@@ -555,13 +555,7 @@ void autonomous(){
 }
 
 void opcontrol(){
-    wings.set_value(true);
-    translate(-3.5_ft);
-    while(translatePID->getError() > 50) {
-        pros::delay(20);
-    }
-    wings.set_value(false);
-    translate(2.5_ft);
+    
     // (chassis->getModel())->arcade(1, 0);
 
     // liftController->reset();
