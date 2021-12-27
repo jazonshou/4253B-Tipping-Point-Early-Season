e475f134a3f910cabeaede526dfe8c98e2e0c81e
Yessir4253 <67040509+Yessir4253@users.noreply.github.com>
2021-12-27 19:41:13 +0800

    ft. added abaittor (aka. path planner "c++ array" convertor to trajectory)

--- src/lib/Convertor.py
+++ src/lib/Convertor.py
@@ -1,11 +1,40 @@
 import pyperclip
 
+# Auton namespaces (uncomment the auton you're working on)
+auton = 'Skills'
+# auton = 'AWP'
+# auton = 'Left'
+# auton = 'Right'
+
+
 out = ''
 
-while True:
-    s = input()
-    out += s + '\n'
-    if s == '};':
-        break
+# Opens the file where the path is stored temporarily
+# Deletes all content of the file after processing
+with open('../paths/abattoir.txt', 'r+') as f:
+    for line in f:
+        out += line
+    f.truncate(0)
 
+# Makes sure user did not accidentally paste the already generated path
+if(out.find('double') == -1):
+    print('u high bro?')
+    quit()
+
+# Replace 'double' with 'Trajectory' 
+out = 'Trajectory' + out[6:]
+
+# Adds auton namespace next to path name
+out = out[:11] + auton + '::' + out[11:]
+
+# Removes '[][]'
+val = out.find('[][]')
+out = out[:val] + out[val+4:]
+
+# Adds semicolon
+out += ';'
+
+# Copies path to clipboard
 pyperclip.copy(out)
+
+print('Path copied to clipboard!!!')
